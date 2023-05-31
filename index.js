3f0bc8c3eb442fae1d09c5ef9078ecca51bddba1
ProgrammerBayzid <mdbayzid2622002@gmaile.com>
2023-05-31 11:55:51 +0600

    2n commit

--- index.js
+++ index.js
@@ -1,75 +1,92 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-const app = express()
-const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion, ObjectId, } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId, } = require("mongodb");
 
 // middelware
-app.use(cors())
-app.use(express.json())
-
-
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.j7rvpzy.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-try {
-    
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
+async function run() {
+  try {
+    const blogsCollection = client.db("bringinBlogs").collection("blogs");
+    const commentCollection = client.db("bringinBlogs").collection("comment");
+    const bringinfeaturedCollection = client.db("bringinBlogs").collection("bringinfeatured");
+    const influencersCollection = client.db("bringinBlogs").collection("influencers ");
+    const cetagoryCollection = client
+      .db("bringinBlogs")
+      .collection("blogCetagory");
 
-    const blogsCollection = client.db('bringinBlogs').collection('blogs');
-    const cetagoryCollection = client.db('bringinBlogs').collection('blogCetagory');
-
-
-
-
-
-
-
-    app.get('/blogs', async (req, res) => {
-        const query = {};
-        const blogs = await blogsCollection.find(query).toArray();
-        res.send(blogs);
+    app.get("/blogs", async (req, res) => {
+      const query = {};
+      const blogs = await blogsCollection.find(query).toArray();
+      res.send(blogs);
     });
-    app.get('/cetagorys', async (req, res) => {
-        const query = {};
-        const cetagory = await cetagoryCollection.find(query).toArray();
-        res.send(cetagory);
-    });
-
-    app.post('/blogs', async (req, res) => {
-        const user = req.body;
-        const result = await blogsCollection.insertOne(user);
-        res.send(result);
+    app.get("/comment", async (req, res) => {
+      const query = {};
+      const comment = await commentCollection.find(query).toArray();
+      res.send(comment);
     });
 
+    app.get("/cetagorys", async (req, res) => {
+      const query = {};
+      const cetagory = await cetagoryCollection.find(query).toArray();
+      res.send(cetagory);
+    });
 
-} catch (error) {
-    
+    app.get("/catagory/blogs/:categoryName", async (req, res) => {
+      const catagory = req.params.categoryName;
+      const catagoryName = await blogsCollection
+        .find({ categoryName: catagory })
+        .toArray();
+      res.send(catagoryName);
+    });
+
+    app.get("/blogs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const blog = await blogsCollection.findOne(query);
+      res.send(blog);
+    });
+
+    app.post("/blogs", async (req, res) => {
+      const user = req.body;
+      const result = await blogsCollection.insertOne(user);
+      res.send(result);
+    });
+    app.post("/comment", async (req, res) => {
+      const user = req.body;
+      const comment = await commentCollection.insertOne(user);
+      res.send(comment);
+    });
+    app.post("/bringinfeatured", async (req, res) => {
+      const user = req.body;
+      const bringinfeatured = await bringinfeaturedCollection.insertOne(user);
+      res.send(bringinfeatured);
+    });
+    app.post("/influencers", async (req, res) => {
+      const user = req.body;
+      const influencers = await influencersCollection.insertOne(user);
+      res.send(influencers);
+    });
+  } catch (error) { }
 }
+run().catch(console.log);
 
-
-}
-run().catch(console.log)
-
-
-
-
-
-
-
-
-
-
-
-
-app.get('/', (req, res) => {
-    res.send('Bringin Blogs server is running')
-})
+app.get("/", (req, res) => {
+  res.send("Bringin Blogs server is running");
+});
 app.listen(port, (req, res) => {
-    console.log(`Bringin Blogs server  server is running port ${port}`);
-})
+  console.log(`Bringin Blogs server  server is running port ${port}`);
+});
